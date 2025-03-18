90ebc7f78f511fdd450b9794f1416482a606b870
Daziem701 <necros701@gmail.com>
2025-03-18 12:09:18 -0400

    commit

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let amigointroducido = 0;
+
+
+function agregarAmigo() {
+    amigointroducido = document.getElementById('amigo').value;
+    if (amigointroducido.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+    }else{
+    
+    
+    amigos.push(amigointroducido);
+    limpiarCaja();  
+    
+
+    }   
+}
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+function RecorreAmigos() {
+
+}
+function recorreramigos('li') {
+    recorreramigos.innerHTML = amigointroducido;
+    
+}
