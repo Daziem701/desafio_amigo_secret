b1f34eb44a162d338bab4c5b8b3cd739158ec1f3
Daziem701 <necros701@gmail.com>
2025-03-18 15:43:48 -0400

    final

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos=[];
+// El principal objetivo de este desafío es fortalecer tus habilidades en 
+// lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; 
 
- /* Funcion para agregar amigos */
+function agregarAmigo() {
+    let amigointroducido = document.getElementById('amigo').value;
+    
+    if (amigointroducido.trim() === "") {
+        alert("Por favor, inserte un nombre válido.");
+    } else {
 
- function agregarAmigo(){
-    nombreAmigo=document.getElementById('amigo').value;
-    if(nombreAmigo==''){
-        alert('Por favor inserte un nombre valido');
-    }else{
-        listaAmigos.push(nombreAmigo);
-        console.log(listaAmigos);
-        listaHTML=document.getElementById('listaAmigos');
-        listaHTML.innerHTML+=`<li>${listaAmigos[listaAmigos.length-1]}</li>`;
+        amigos.push(amigointroducido);
+        console.log(amigos);
+        limpiarCaja();
+        Recorre_Array(); 
+
     }
- limpiarCajaTexto();
+}
 
- }
- /* Funcion para sortear amigo */
-  function sortearAmigo(){
-    let numeroAleatorio=Math.floor(Math.random()*listaAmigos.length-1)+1;
-    console.log(numeroAleatorio);
-    resultadoHTML=document.getElementById('resultado');
-    resultadoHTML.innerHTML=`El amigo secreto es: ${listaAmigos[numeroAleatorio]}`;
-  }
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
 
+function Recorre_Array() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
 
-  /* Funcion limpiar */
-  function limpiarCajaTexto(){
-    document.querySelector('#amigo').value = '';
-  }
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado'); 
+    if (amigos.length === 0) {
+        resultado.innerHTML = "No tienes amigos en la lista.";
+    } else {
+        
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        console.log (indiceAleatorio);
+        let amigoSorteado = amigos[indiceAleatorio];   
+        resultado.innerHTML = "El amigo sorteado es: " + amigoSorteado;
+    }
+   
+}
